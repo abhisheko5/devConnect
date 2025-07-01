fc83d25fd0506cb0cb1fa95a455354f9854d14d8
abhisheko5 <86157818+abhisheko5@users.noreply.github.com>
2025-07-01 15:17:03 +0530

    Add CORS support and enhance security with helmet in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import cors from "cors";
 const app = express();
-
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+
+
+app.use(cors({
+  origin: true, // or set your frontend origin
+  credentials: true,
+}));
+app.get("/", (req, res) => {
+  res.send("🚀 DevConnect API is Live");
+});
+
 
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
